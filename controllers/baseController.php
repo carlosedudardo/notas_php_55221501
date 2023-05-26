434cef8480785678029ffc232644831f5cdf5256
carlosedudardo <carestupinan@uniboyaca.edu.co>
2023-05-26 16:34:26 -0500

    se agregan controladores, basecontroller y la conexion con la base de datos

---
+++ controllers/baseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace baseControler;
+
+abstract class BaseController
+{
+    abstract function create($model);
+    abstract function read();
+    abstract function update($codigo, $model);
+    abstract function delete($codigo);
+    abstract function readRow($codigo);
+}
+
+abstract class ActividadBaseController
+{
+    abstract function create($model);
+    abstract function read($codigo);
+    abstract function update($id, $model);
+    abstract function delete($id);
+    abstract function readRow($id);
+}
--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+</head>
+<body>
+    <main>
+        <h1>LISTA DE LOS ESTUDIANTES</h1>
+        <table>
+            
+        </table>
+    </main>
+    
+</body>
+</html>
